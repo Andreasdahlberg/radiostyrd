002d1d3de7759580b496b128cd72ed81c747bcc7
Andreas Dahlberg <andreas.dahlberg90@gmail.com>
2020-12-30 20:24:25 +0100

    Make speed controller thread safe

--- main/speedcontroller.c
+++ main/speedcontroller.c
@@ -23,7 +23,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <assert.h>
+#include <math.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
+
 #include "utils.h"
 #include "speedcontroller.h"
 
@@ -34,16 +41,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #define MAX_NUMBER_OF_INPUTS 3
 #define MAX_NUMBER_OF_LISTENERS 2
 #define MAX_INPUT_VALUE 1.0
-
-///////////////////////////////////////////////////////////////////////////////
-//FUNCTION PROTOTYPES
-///////////////////////////////////////////////////////////////////////////////
-
-static inline size_t get_active_id(void);
-static inline const char* get_active_name(void);
-static inline float get_output_value(void);
-static void select_active_input(void);
-static void notify_listeners(void);
+#define MAX_QUEUE_ITEMS 64
+#define CORE_ID 1
 
 ///////////////////////////////////////////////////////////////////////////////
 //TYPES
@@ -54,20 +53,40 @@ struct input_t {
   const char *name_p;
 };
 
+struct input_message_t
+{
+  size_t id;
+  float value;
+};
+
 struct speedcontroller_t {
   struct input_t inputs[MAX_NUMBER_OF_INPUTS];
   size_t number_of_inputs;
   size_t active_input_id;
   speedcontroller_callback listeners[MAX_NUMBER_OF_LISTENERS];
   size_t number_of_listeners;
+  enum speedcontroller_direction_t direction;
+  QueueHandle_t message_queue;
+  SemaphoreHandle_t mutex;
 };
+
+///////////////////////////////////////////////////////////////////////////////
+//FUNCTION PROTOTYPES
+///////////////////////////////////////////////////////////////////////////////
+
+static void controller_task(void * parameter);
+static void handle_input_message(const struct input_message_t *message_p);
+static inline size_t get_active_id(void);
+static inline const char* get_active_name(void);
+static inline float get_output_value(void);
+static void select_active_input(void);
+static void notify_listeners(void);
 
 ///////////////////////////////////////////////////////////////////////////////
 //VARIABLES
 ///////////////////////////////////////////////////////////////////////////////
 
 static const char* TAG = "speedcontroller";
-
 static struct speedcontroller_t speedcontroller;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -77,54 +96,104 @@ static struct speedcontroller_t speedcontroller;
 void speedcontroller_init(void)
 {
   speedcontroller = (typeof(speedcontroller)) {0};
+  speedcontroller.mutex = xSemaphoreCreateMutex();
+  speedcontroller.message_queue = xQueueCreate(MAX_QUEUE_ITEMS, sizeof(struct input_message_t));
+  assert(speedcontroller.message_queue != NULL);
+
+  TaskHandle_t xHandle = NULL;
+  xTaskCreatePinnedToCore(controller_task, "Controller", 2048, NULL, tskIDLE_PRIORITY, &xHandle, CORE_ID);
+  assert(xHandle != NULL);
 
   ESP_LOGI(TAG, "Initialized");
 }
 
 size_t speedcontroller_register_input(const char *name_p)
 {
+  xSemaphoreTake(speedcontroller.mutex, portMAX_DELAY);
+
   assert(name_p != NULL);
   assert(speedcontroller.number_of_inputs < ElementsIn(speedcontroller.inputs));
 
   const size_t input_id = speedcontroller.number_of_inputs;
   speedcontroller.inputs[input_id].name_p = name_p;
-  speedcontroller.inputs[input_id].value = MAX_INPUT_VALUE;
+  speedcontroller.inputs[input_id].value = 0.0;
   ++speedcontroller.number_of_inputs;
 
-  ESP_LOGI(TAG, "Registered input '%s' with ID %zu", name_p, input_id);
+  xSemaphoreGive(speedcontroller.mutex);
 
+  ESP_LOGI(TAG, "Registered input '%s' with ID %zu", name_p, input_id);
   return input_id;
 }
 
 void speedcontroller_register_listener(speedcontroller_callback listener)
 {
+  xSemaphoreTake(speedcontroller.mutex, portMAX_DELAY);
+
   assert(listener != NULL);
   assert(speedcontroller.number_of_listeners < ElementsIn(speedcontroller.listeners));
 
   const size_t index = speedcontroller.number_of_listeners;
   speedcontroller.listeners[index] = listener;
   ++speedcontroller.number_of_listeners;
-}
 
-float speedcontroller_get_output(void)
-{
-  return get_output_value();
+  xSemaphoreGive(speedcontroller.mutex);
+
+  ESP_LOGI(TAG, "Registered listener with callback 0x%x", (uint32_t)listener);
 }
 
 void speedcontroller_input(size_t id, float value)
 {
   assert(id < ElementsIn(speedcontroller.inputs));
 
-  speedcontroller.inputs[id].value = value;
+  const struct input_message_t message = {.id = id, .value = value};
 
-  select_active_input();
-  notify_listeners();
+  if (xQueueSendToBack(speedcontroller.message_queue, &message, 100 / portTICK_PERIOD_MS) != pdTRUE)
+  {
+    ESP_LOGE(TAG, "Failed to set input '%zu' to '%f'", id, value);
+  }
+}
+
+void speedcontroller_set_direction(enum speedcontroller_direction_t direction)
+{
+  assert(direction < SPEEDCONTROLLER_MAX);
+  speedcontroller.direction = direction;
+}
+
+enum speedcontroller_direction_t speedcontroller_get_direction(void)
+{
+  return speedcontroller.direction;
+}
+
+const char *speedcontroller_get_name(size_t id)
+{
+  assert(id < ElementsIn(speedcontroller.inputs));
+
+  return speedcontroller.inputs[id].name_p;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //LOCAL FUNCTIONS
 ///////////////////////////////////////////////////////////////////////////////
 
+static void controller_task(void * parameter)
+{
+  while (1)
+  {
+    struct input_message_t message;
+    xQueueReceive(speedcontroller.message_queue, &message, portMAX_DELAY);
+
+    ESP_LOGV(TAG, "New input: %s(%zu) -> %f", speedcontroller_get_name(message.id), message.id, message.value);
+    handle_input_message(&message);
+  }
+}
+
+static void handle_input_message(const struct input_message_t *message_p)
+{
+  /* Sign can be removed since the direction is set separately. */
+  speedcontroller.inputs[message_p->id].value = fabsf(message_p->value);
+  select_active_input();
+  notify_listeners();
+}
 
 static inline size_t get_active_id(void)
 {
@@ -157,18 +226,14 @@ static void select_active_input(void)
 
   speedcontroller.active_input_id = active_input_id;
 
-  ESP_LOGI(TAG, "Selected input '%s' with ID %zu", get_active_name(), get_active_id());
+  ESP_LOGV(TAG, "Selected input '%s' with ID %zu", get_active_name(), get_active_id());
 }
 
 static void notify_listeners(void)
 {
-  for (size_t i = 0; i < ElementsIn(speedcontroller.listeners); ++i)
+  for (size_t i = 0; i < speedcontroller.number_of_listeners; ++i)
   {
-    if (speedcontroller.listeners[i] == NULL)
-    {
-      break;
-    }
-
+    assert(speedcontroller.listeners[i] != NULL);
     speedcontroller.listeners[i](get_active_id(), get_output_value());
   }
 }
--- main/speedcontroller.h
+++ main/speedcontroller.h
@@ -9,7 +9,14 @@
 //TYPES
 ///////////////////////////////////////////////////////////////////////////////
 
+/**
+ * Called when the output is changed.
+ *
+ * @param id ID of the input selected as output.
+ * @param value Output value
+ */
 typedef void (*speedcontroller_callback)(size_t id, float value);
+enum speedcontroller_direction_t {SPEEDCONTROLLER_FORWARD, SPEEDCONTROLLER_REVERSE, SPEEDCONTROLLER_MAX};
 
 ///////////////////////////////////////////////////////////////////////////////
 //FUNCTION PROTOTYPES
@@ -39,18 +46,34 @@ size_t speedcontroller_register_input(const char *name_p);
 void speedcontroller_register_listener(speedcontroller_callback listener);
 
 /**
- * Get the speed selected by the controller.
- *
- * @return Speed
- */
-float speedcontroller_get_output(void);
-
-/**
  * Set value of the specified input.
  *
  * @param id ID of the input, see 'speedcontroller_register_input()'.
  * @param value New value of the input.
  */
 void speedcontroller_input(size_t id, float value);
+
+/**
+ * Set the direction.
+ *
+ * @param direction
+ */
+void speedcontroller_set_direction(enum speedcontroller_direction_t direction);
+
+/**
+ * Get the direction.
+ *
+ * @return The direction, SPEEDCONTROLLER_FORWARD or SPEEDCONTROLLER_REVERSE.
+ */
+enum speedcontroller_direction_t speedcontroller_get_direction(void);
+
+/**
+ * Get the name of the specified input.
+ *
+ * @param id ID of input.
+ *
+ * @return Name of input.
+ */
+const char *speedcontroller_get_name(size_t id);
 
 #endif
